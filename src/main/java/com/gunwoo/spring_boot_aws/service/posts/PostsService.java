6c86c11da6b594079f9c5fa5cf194bc1790bb6c4
kgwoo <gunoo0126@gmail.com>
2024-05-18 00:22:10 +0900

    log

--- src/main/java/com/gunwoo/spring_boot_aws/service/posts/PostsService.java
+++ src/main/java/com/gunwoo/spring_boot_aws/service/posts/PostsService.java
@@ -28,9 +28,9 @@ public class PostsService {
         return id;
     }
 
+    @Transactional
     public PostsResponseDto findById (Long id){
         Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
         return new PostsResponseDto(entity);
     }
-
 }
